5d2d4d067eb5cc02ec60e30d65f04eaf1cef36a2
Jongyup <coolmax0909@outlook.com>
2023-01-14 16:23:29 +0900

    JS 101

--- exercise/test.js
+++ exercise/test.js
@@ -1,4 +1,70 @@
+//alert("Hi");
+
 console.log("hello");
 
 const a = "NeverChange";
 let b = "CanChanged";
+
+// boolean
+// true, false. null, undefined
+
+//arrays
+const primeNums = [2, 3, 5,];
+console.log(primeNums);
+console.log(primeNums[2]);
+primeNums.push(7);
+console.log(primeNums);
+
+//objects
+/*
+const userName = "JY";
+const userAge = 25;
+const userGirlfriend = null;
+*/
+const user = {
+    name: "JY",
+    age: 25,
+    girlfriend: null
+};
+
+console.log(user.name);
+user.hobby = "game";
+console.log(user);
+
+//function
+function sayHello() {
+    console.log("Hello");
+}
+sayHello();
+
+function myNameIs(name) {
+    console.log("My name is", name)
+};
+myNameIs(user.name);
+
+function add(a, b) {
+    return a+b;
+}
+console.log(add(1,2));
+
+//conditions
+//parseInt() _ sting into number
+//prompt() _ take Input
+//isNaN() _ check if it's Not a Number!
+function isItTrue(condition) {
+    if(condition == true) {
+        console.log("It's True");
+    }
+    else {
+        console.log("It's False");
+    }
+}
+isItTrue(10 > 5);
+isItTrue(5 > 10);
+isItTrue(5 == 10);
+
+console.log(isNaN(user.age));
+console.log(isNaN(user.name));
+
+// && and operator
+// || or operator
