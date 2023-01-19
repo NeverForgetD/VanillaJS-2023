cf1cfcc60b6bfeed2c946c100c50d7a718e793c6
Jongyup <coolmax0909@outlook.com>
2023-01-19 19:45:32 +0900

    img.js (not using)

---
+++ momentum/js/img.js
@@ -0,0 +1,10 @@
+/*
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+const imgNum = Math.floor(Math.random()*images.length);
+const img = images[imgNum];
+
+const bgImg = document.createElement("img");
+bgImg.src = `img/${img}`;
+
+document.body.appendChild(bgImg);
+*/
