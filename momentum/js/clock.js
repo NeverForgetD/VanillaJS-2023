59adc9eb653a5a1409f924dda8aba8bcf093baae
Jongyup <coolmax0909@outlook.com>
2023-01-19 17:47:42 +0900

    clock updates (with ENG month)

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,3 +1,60 @@
 const clock = document.querySelector(".clock");
+/*
+function hello() {
+    console.log("tiktok");
+}
+setInterval(hello, 1000);
+setTimeout(hello, 1000);
+*/
+function monthGen(i) {
+    if (i === 0) {
+        return "JAN";
+    }
+    if (i === 1) {
+        return "FEB";
+    }
+    if (i === 2) {
+        return "MAR";
+    }
+    if (i === 3) {
+        return "APR";
+    }
+    if (i === 4) {
+        return "MAY";
+    }
+    if (i === 5) {
+        return "JUN";
+    }
+    if (i === 6) {
+        return "JUL";
+    }
+    if (i === 7) {
+        return "AUG";
+    }
+    if (i === 8) {
+        return "SEP";
+    }
+    if (i === 9) {
+        return "OCT";
+    }
+    if (i === 10) {
+        return "NOV";
+    }
+    if (i === 11) {
+        return "DEC";
+    } 
+}
 
-clock.innerText = "hi";
+function showTime() {
+    const time = new Date();
+    const hour = String(time.getHours()).padStart(2, "0");
+    const minute = String(time.getMinutes()).padStart(2, "0");
+    const monthCode = time.getMonth();
+    const month = monthGen(monthCode);
+    const second = String(time.getSeconds()).padStart(2, "0");
+    const date = String(time.getDate());
+    clock.innerText = `${hour}:${minute}:${second} ${month} ${date}`
+}
+
+showTime();
+setInterval(showTime, 1000);
