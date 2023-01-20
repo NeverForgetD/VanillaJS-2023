581c5bf36792e3b39dd7c17ffdbc4a809c15647c
Jongyup <coolmax0909@outlook.com>
2023-01-20 15:08:30 +0900

    confirm reset

--- momentum/js/reset.js
+++ momentum/js/reset.js
@@ -1,6 +1,9 @@
 const resetBtn = document.querySelector(".reset-btn");
 
 function handleReset() {
-    localStorage.clear();
+    let reallyReset = confirm("정말로 리셋하시겠습니까?");
+    if (reallyReset) {
+        localStorage.clear();
+    }
 }
 resetBtn.addEventListener("click", handleReset);
